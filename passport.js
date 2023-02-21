58b6407995a0e64524f30d4c2626911ae5a2e45e
mucacran <el.mucacran.rasta@hotmail.com>
2023-02-21 13:22:22 -0500

    nuevos cambios

--- passport.js
+++ passport.js
@@ -11,7 +11,7 @@ module.exports = function (passport) {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: 'http://localhost:8080/auth/google/callback',
+        callbackURL: 'https://week7tarea.onrender.com/auth/google/callback',
       },
       async (accessToken, refreshToken, profile, done) => {
         const newUser = {
